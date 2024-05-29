151767d492f36fbcd552ba4908a55b3f6bce092c
Vibhu Gupta <vikram.kr.gupta01@gmail.com>
2024-05-29 17:30:44 +0530

    color config for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,14 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        primary: "#160647",
+        secondary: "#6C6C6C",
+        third :  "#47BEBA"
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
