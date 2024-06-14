dd72ff7b2416642558d2487ee21ca192887cb9fe
Vibhu Gupta <vikram.kr.gupta01@gmail.com>
2024-06-15 01:36:10 +0530

    some chnages

--- app/lib/getCategories.js
+++
@@ -1,7 +0,0 @@
-export default async function getCategories() {
-    const response = await fetch("https://api.escuelajs.co/api/v1/categories");
-
-    const categories = await response.json();
-
-    return categories;
-}
