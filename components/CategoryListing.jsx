55dde6c10a314e2a1e570a759dbf4a12a96f978f
Vibhu Gupta <vikram.kr.gupta01@gmail.com>
2024-06-14 13:31:26 +0530

    Category of storeHub is added

--- components/CategoryListing.jsx
+++ components/CategoryListing.jsx
@@ -1,30 +1,54 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const CategoryListing = () => {
+const CategoryListing = ({ data }) => {
+  // console.log('Data received:', data); 
+
+  // Check if data.categories exists and is an array
+  const categories = Array.isArray(data.categories) ? data.categories : [];
+  // console.log(categories)
+
   return (
-    <section className='flex flex-col gap-9'>
+    <section className='justify-center items-center flex flex-col  gap-10'>
       <div>
         <h1 className="text-5xl font-bold items-center flex">Category Listing</h1>
       </div>
-      <div className="grid grid-cols-4">
-        <Link href="#" className=''>
-          <div>
-            <Image
-              src="/json/categories/television.jpg"
-              width={180}
-              height={180}
-              alt='television-images'
-            />
-          </div>
-          <h1>Television</h1>
-        </Link>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4  gap-20 m-9 ">
+        {categories.map((category) => (
+          <Link 
+            href="#" 
+            className=' w-auto p-3 rounded-md shadow-lg '
+            key={category.id}
+          >
+            <div className=' w-[16rem] h-[15rem]  relative  '>
+              <Image
+                src={category.image}
+                fill
+                alt={`${category.title} image`}
+              />
+            </div>
+            <h1 className='flex justify-center items-center  pt-4 pb-4 text-3xl  font-bold '>{category.title}</h1>
+          </Link>
+        ))}
       </div>
     </section>
   );
 };
 
 CategoryListing.displayName = 'CategoryListing';
+
+// CategoryListing.propTypes = {
+//   data: PropTypes.shape({
+//     categories: PropTypes.arrayOf(PropTypes.shape({
+//       id: PropTypes.number.isRequired,
+//       title: PropTypes.string.isRequired,
+//       description: PropTypes.string.isRequired,
+//       image: PropTypes.string.isRequired,
+//       key: PropTypes.string.isRequired,
+//     })).isRequired,
+//   }).isRequired,
+// };
 
 export default CategoryListing;
